07158c54902bc9d6b6015df7c1b3923226f70c4b
machmak <91174788+machmak@users.noreply.github.com>
2021-10-02 09:20:13 +0700

    login and logout done

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,7 +5,7 @@ if(Session::has('user'))
 {
   $total= ProductController::cartItem();
 }
-
+55555545
 ?>
 <nav class="navbar navbar-default">
     <div class="container-fluid">
@@ -27,14 +27,25 @@ if(Session::has('user'))
           <li><a href="/myorders">Orders</a></li>
        
         </ul>
-        <form class="navbar-form navbar-left">
+        <form action="/search" class="navbar-form navbar-left">
           <div class="form-group">
-            <input type="text" class="form-control " placeholder="Search">
+            <input type="text" name="query" class="form-control search-box " placeholder="Search">
           </div>
-          <button type="submit" class="btn btn-default">Sumit</button>
+          <button type="submit" class="btn btn-default">Search</button>
         </form>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Add To Cart</a></li>
+        <li><a href="/cartlist">cart({{$total}})</a></li>
+        @if(Session::has('user'))
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="/logout">Logout</a></li>
+            </ul>
+          </li>
+          @else
+          <li><a href="/login">Login</a></li>
+          @endif
       
         </ul>
       </div><!-- /.navbar-collapse -->
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,7 +14,7 @@
                 <label for="exampleInputPassword1">Password</label>
                 <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
-                <button type="submit" class="btn btn-default">Login</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
