2f23f063cc7a4b414906ae030d4b4302ad8eb5ea
machmak <91174788+machmak@users.noreply.github.com>
2021-10-02 09:55:55 +0700

    remove form cart done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,8 +7,7 @@ use App\Models\Product;
 use App\Models\Cart;
 
 use Session;
-
-
+use Illuminate\Support\Facades\DB;
 class ProductController extends Controller
 {
     //
@@ -50,6 +49,22 @@ class ProductController extends Controller
     {
     $userId=Session::get('user')['id'];
     return Cart::where('user_id',$userId)->count(); 
+    }
+    function cartList()
+    {
+        $userId=Session::get('user')['id'];
+        $products= DB::table('cart')
+         ->join('products','cart.product_id','=','products.id')
+         ->where('cart.user_id',$userId)
+         ->select('products.*','cart.id as cart_id')
+         ->get();
+ 
+         return view('cartlist',['products'=>$products]);   
+    }
+    function removeCart($id)
+    {
+        Cart::destroy($id);
+        return redirect('cartlist');
     }
 }
 
